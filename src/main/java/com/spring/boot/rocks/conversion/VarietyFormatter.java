babfe853744d89d338fb4563654e3dfc94a894be
ajay <ajay@linuxpc>
2020-12-24 23:41:11 -0500

    initial

--- src/main/java/com/spring/boot/rocks/conversion/VarietyFormatter.java
+++ src/main/java/com/spring/boot/rocks/conversion/VarietyFormatter.java
@@ -1,22 +1,3 @@
-/*
- * =============================================================================
- * 
- *   Copyright (c) 2011-2016, The THYMELEAF team (http://www.thymeleaf.org)
- * 
- *   Licensed under the Apache License, Version 2.0 (the "License");
- *   you may not use this file except in compliance with the License.
- *   You may obtain a copy of the License at
- * 
- *       http://www.apache.org/licenses/LICENSE-2.0
- * 
- *   Unless required by applicable law or agreed to in writing, software
- *   distributed under the License is distributed on an "AS IS" BASIS,
- *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *   See the License for the specific language governing permissions and
- *   limitations under the License.
- * 
- * =============================================================================
- */
 package com.spring.boot.rocks.conversion;
 
 import java.text.ParseException;
@@ -29,27 +10,25 @@ import org.springframework.stereotype.Component;
 import com.spring.boot.rocks.entities.Variety;
 import com.spring.boot.rocks.services.VarietyService;
 
-
 @Component
 public class VarietyFormatter implements Formatter<Variety> {
 
-    @Autowired
-    private VarietyService varietyService;
+	@Autowired
+	private VarietyService varietyService;
 
+	public VarietyFormatter() {
+		super();
+	}
 
-    public VarietyFormatter() {
-        super();
-    }
+	public Variety parse(final String text, final Locale locale) throws ParseException {
+		final Integer varietyId = Integer.valueOf(text);
+		// There is no Formatter API yet that allows us to return a Publisher, so we
+		// need to block
+		return this.varietyService.findById(varietyId).block();
+	}
 
-    public Variety parse(final String text, final Locale locale) throws ParseException {
-        final Integer varietyId = Integer.valueOf(text);
-        // There is no Formatter API yet that allows us to return a Publisher, so we need to block
-        return this.varietyService.findById(varietyId).block();
-    }
-
-
-    public String print(final Variety object, final Locale locale) {
-        return (object != null ? object.getId().toString() : "");
-    }
+	public String print(final Variety object, final Locale locale) {
+		return (object != null ? object.getId().toString() : "");
+	}
 
 }
--- src/main/java/com/spring/boot/rocks/services/VarietyService.java
+++ src/main/java/com/spring/boot/rocks/services/VarietyService.java
@@ -1,22 +1,3 @@
-/*
- * =============================================================================
- * 
- *   Copyright (c) 2011-2016, The THYMELEAF team (http://www.thymeleaf.org)
- * 
- *   Licensed under the Apache License, Version 2.0 (the "License");
- *   you may not use this file except in compliance with the License.
- *   You may obtain a copy of the License at
- * 
- *       http://www.apache.org/licenses/LICENSE-2.0
- * 
- *   Unless required by applicable law or agreed to in writing, software
- *   distributed under the License is distributed on an "AS IS" BASIS,
- *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *   See the License for the specific language governing permissions and
- *   limitations under the License.
- * 
- * =============================================================================
- */
 package com.spring.boot.rocks.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -30,23 +11,20 @@ import reactor.core.publisher.Mono;
 
 @Service
 public class VarietyService {
-    
-    @Autowired
-    private VarietyRepository varietyRepository;
-    
-    
-    public VarietyService() {
-        super();
-    }
-    
-    
-    
-    public Flux<Variety> findAll() {
-        return this.varietyRepository.findAll();
-    }
 
-    public Mono<Variety> findById(final Integer id) {
-        return this.varietyRepository.findById(id);
-    }
-    
+	@Autowired
+	private VarietyRepository varietyRepository;
+
+	public VarietyService() {
+		super();
+	}
+
+	public Flux<Variety> findAll() {
+		return this.varietyRepository.findAll();
+	}
+
+	public Mono<Variety> findById(final Integer id) {
+		return this.varietyRepository.findById(id);
+	}
+
 }
